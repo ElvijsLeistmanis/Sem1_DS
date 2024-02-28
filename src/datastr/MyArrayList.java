97b5c070f76f2ef518a9fb17314570364d314067
Elvijs Leistmanis <s22leiselvi@vea.lv>
2024-02-28 10:12:07 +0200

    Add: resize function

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -6,7 +6,6 @@ public class MyArrayList {
 	private int size = LIST_DEFAULT_SIZE;
 	private int counter = 0;
 	
-	//TODO
 	//Constructors
 	public MyArrayList () {
 		list = new int[size];
@@ -19,19 +18,32 @@ public class MyArrayList {
 		list = new int[size];
 	}
 	
-	//isEmpty
+	//Check if list is empty
 	public boolean isEmpty() {
 		//return (counter == 0) ? true : false;
 		return counter == 0;
 	}
 	
-	//isFull
+	//Check if list is full
 	public boolean isFull() {
 		return counter == size;
 	}
 	
-	//howManyElements
+	//Check list element count
 	public int howManyElements() {
 		return counter;
+	}
+	
+	//Increase list size
+	private void resize() {
+		int newSize = (counter <= 100) ? size * 2 : (int)(size * 1.5);
+		int[] newList = new int[newSize];
+		
+		for(int i = 0; i < size; i++) {
+			newList[i] = list[i];
+		}
+		
+		list = newList;
+		System.gc();
 	}
 }
